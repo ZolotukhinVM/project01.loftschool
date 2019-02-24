df5ae85e1ce1b1ed5114cf082acdc0a3b23d9c58
Viktor Zolotukhin <zolotukhinvm@ya.ru>
2019-02-24 12:42:07 +0300

    #4 update after comments d.rudenskiy

--- dbconnect.php
+++ dbconnect.php
@@ -2,6 +2,6 @@
 try {
     $dbConnect = new PDO("mysql:host=localhost;dbname=project01_db", "root", "");
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo "Error: " . $e->getMessage();
     die;
 }
--- order.php
+++ order.php
@@ -1,49 +1,49 @@
 <?php
 require 'dbconnect.php';
 $email = trim($_POST["email"]);
-$resUsers = $dbConnect->query("SELECT `id` FROM `users_tbl` WHERE `email` = '" . $email . "'");
-if ($resUsers->rowCount() == 0) {
-    $userNew = "Y";
+try {
     $dbConnect->beginTransaction();
-//    $insertUser = $dbConnect->query("INSERT INTO `users_tbl` (`name`, `phone`, `email`) VALUES ('" . $_POST["name"] . "', '" . $_POST["phone"] . "', '" . $_POST["email"] . "')");
-    $sqlInsertUser = "INSERT INTO `users_tbl` (`name`, `phone`, `email`) VALUES (?, ?, ?)";
-    $insertUser = $dbConnect->prepare($sqlInsertUser);
-    $insertUser->bindParam(1, $name);
-    $insertUser->bindParam(2, $phone);
-    $insertUser->bindParam(3, $email);
-    $name = $_POST["name"];
-    $phone = $_POST["phone"];
-    $email = $_POST["email"];
-    $resUser = $insertUser->execute();
-} else {
-    $userNew = "N";
-    $user = $resUsers->fetch(PDO::FETCH_ASSOC);
-}
-$sqlInsertOrder = "INSERT INTO `orders_tbl` (`id_user`, `address`, `comment`, `pay`, `callback`) VALUES (?, ?, ?, ?, ?)";
-$insertOrder = $dbConnect->prepare($sqlInsertOrder);
-$insertOrder->bindParam(1, $userId, PDO::PARAM_INT);
-$insertOrder->bindParam(2, $address);
-$insertOrder->bindParam(3, $comment);
-$insertOrder->bindParam(4, $pay);
-$insertOrder->bindParam(5, $callback);
-//todo lastIsertId change for SELECT * FROM TABLE ORDER BY DESC
-$userId = ($userNew == "Y") ? $dbConnect->lastInsertId() : $user["id"];
-$address = $_POST["street"] . " / " . $_POST["home"] . " / " . $_POST["part"] . " / " . $_POST["appt"] . " / " . $_POST["floor"];
-$comment = $_POST["comment"];
-$pay = $_POST["pay"];
-$callback = (isset($_POST["callback"])) ? "N" : "Y";
-$resOrder = $insertOrder->execute();
-//todo beginTransaction() & commit() + rollback whith try/catch
-if ((isset($resUser) && $resUser) && $resOrder) {
+    $sqlUsers = "SELECT `id` FROM `users_tbl` WHERE `email` = '" . $email . "'";
+    $resUsers = $dbConnect->query($sqlUsers);
+    if ($resUsers->rowCount() == 0) {
+        $userNew = "Y";
+        $sqlInsertUser = "INSERT INTO `users_tbl` (`name`, `phone`, `email`) VALUES (?, ?, ?)";
+        $insertUser = $dbConnect->prepare($sqlInsertUser);
+        $insertUser->bindParam(1, $name);
+        $insertUser->bindParam(2, $phone);
+        $insertUser->bindParam(3, $email);
+        $name = $_POST["name"];
+        $phone = $_POST["phone"];
+        $email = $_POST["email"];
+        $resUser = $insertUser->execute();
+        $userNewId = $dbConnect->query($sqlUsers)->fetch(PDO::FETCH_ASSOC);
+    } else {
+        $userNew = "N";
+        $user = $resUsers->fetch(PDO::FETCH_ASSOC);
+    }
+    $sqlInsertOrder = "INSERT INTO `orders_tbl` (`id_user`, `address`, `comment`, `pay`, `callback`) VALUES (?, ?, ?, ?, ?)";
+    $insertOrder = $dbConnect->prepare($sqlInsertOrder);
+    $insertOrder->bindParam(1, $userId, PDO::PARAM_INT);
+    $insertOrder->bindParam(2, $address);
+    $insertOrder->bindParam(3, $comment);
+    $insertOrder->bindParam(4, $pay);
+    $insertOrder->bindParam(5, $callback);
+    $userId = ($userNew == "Y") ? $userNewId["id"] : $user["id"];
+    $address = $_POST["street"] . " / " . $_POST["home"] . " / " . $_POST["part"] . " / " . $_POST["appt"] . " / " . $_POST["floor"];
+    $comment = $_POST["comment"];
+    $pay = $_POST["pay"];
+    $callback = (isset($_POST["callback"])) ? "N" : "Y";
+    $resOrder = $insertOrder->execute();
     $dbConnect->commit();
-} else {
-    $dbConnect->rollBack();
+} catch (Exception $e) {
+    $dbConnect->rollback();
+    echo "Error: " . $e->getMessage();
 }
-//todo lastIsertId change for SELECT * FROM TABLE ORDER BY DESC
-$nameOrderFile = $dbConnect->lastInsertId() . ".txt";
+$orderLastId = $dbConnect->query("SELECT `id` FROM orders_tbl WHERE `id_user` = '" . $userId . "' ORDER BY `id` DESC")->fetch(PDO::FETCH_ASSOC);
+$nameOrderFile = "orderid_" . $orderLastId["id"] . ".txt";
 $textOrder = "Ваш заказ будет доставлен по адресу: " . $address . "\n";
 $textOrder .= "DarkBeefBurger за 500 рублей, 1 шт. \n";
-//todo SELECT COUNT()
-$textOrder .= "Спасибо - это ваш " . $dbConnect->query("SELECT `id` FROM `orders_tbl` WHERE `id_user` = '" . $userId . "'")->rowCount() . " заказ";
+$countOrders = $dbConnect->query("SELECT COUNT(*) as count FROM `orders_tbl` WHERE `id_user` = '" . $userId . "'")->fetch(PDO::FETCH_ASSOC);
+$textOrder .= "Спасибо - это ваш " . $countOrders["count"] . " заказ";
 file_put_contents("./orders/" . $nameOrderFile, $textOrder);
 echo "<h1>Your order is submited! Please wait!</h1>";
