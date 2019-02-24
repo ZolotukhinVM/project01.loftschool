1d4e4c596df541821ab25ee4e2dd27fbaa785aef
Viktor Zolotukhin <zolotukhinvm@ya.ru>
2019-02-24 20:21:35 +0300

    #7 create functions

---
+++ admin/functions.php
@@ -0,0 +1,38 @@
+<?php
+function insertUser(array $arrPost, PDO $dbConnect)
+{
+    $sqlInsertUser = "INSERT INTO `users_tbl` (`name`, `phone`, `email`) VALUES (?, ?, ?)";
+    $insertUser = $dbConnect->prepare($sqlInsertUser);
+    $insertUser->bindParam(1, $arrPost["name"]);
+    $insertUser->bindParam(2, $arrPost["phone"]);
+    $insertUser->bindParam(3, $arrPost["email"]);
+    $insertUser->execute();
+}
+
+function getAddress($arrPost)
+{
+    return $arrPost["street"] . " / " . $arrPost["home"] . " / " . $arrPost["part"] . " / " . $arrPost["appt"] . " / " . $arrPost["floor"];
+}
+
+function insertOrder(array $arrPost, $userId, PDO $dbConnect)
+{
+    $callback = (isset($arrPost["callback"])) ? "N" : "Y";
+    $sqlInsertOrder = "INSERT INTO `orders_tbl` (`id_user`, `address`, `comment`, `pay`, `callback`) VALUES (?, ?, ?, ?, ?)";
+    $insertOrder = $dbConnect->prepare($sqlInsertOrder);
+    $insertOrder->bindParam(1, $userId, PDO::PARAM_INT);
+    $insertOrder->bindParam(2, getAddress($arrPost));
+    $insertOrder->bindParam(3, $arrPost["comment"]);
+    $insertOrder->bindParam(4, $arrPost["pay"]);
+    $insertOrder->bindParam(5, $callback);
+    $insertOrder->execute();
+}
+
+function putFileOrder($orderId, $userId, PDO $dbConnect)
+{
+    $nameOrderFile = "orderid_" . $orderId . ".txt";
+    $textOrder = "Ваш заказ будет доставлен по адресу: " . getAddress($_POST) . "\n";
+    $textOrder .= "DarkBeefBurger за 500 рублей, 1 шт. \n";
+    $countOrders = $dbConnect->query("SELECT COUNT(*) as count FROM `orders_tbl` WHERE `id_user` = '" . $userId . "'")->fetch(PDO::FETCH_ASSOC);
+    $textOrder .= "Спасибо - это ваш " . $countOrders["count"] . " заказ";
+    file_put_contents("./orders/" . $nameOrderFile, $textOrder);
+}
--- order.php
+++ order.php
@@ -1,45 +1,20 @@
 <?php
-require 'dbconnect.php';
-$email = trim($_POST["email"]);
+require "dbconnect.php";
+require "./admin/functions.php";
 try {
     $dbConnect->beginTransaction();
-    $sqlUsers = "SELECT `id` FROM `users_tbl` WHERE `email` = '" . $email . "'";
+    $sqlUsers = "SELECT `id` FROM `users_tbl` WHERE `email` = '" . trim($_POST["email"]) . "'";
     $resUsers = $dbConnect->query($sqlUsers);
     if ($resUsers->rowCount() == 0) {
-        $sqlInsertUser = "INSERT INTO `users_tbl` (`name`, `phone`, `email`) VALUES (?, ?, ?)";
-        $insertUser = $dbConnect->prepare($sqlInsertUser);
-        $insertUser->bindParam(1, $name);
-        $insertUser->bindParam(2, $phone);
-        $insertUser->bindParam(3, $email);
-        $name = $_POST["name"];
-        $phone = $_POST["phone"];
-        $email = $_POST["email"];
-        $resUser = $insertUser->execute();
+        insertUser($_POST, $dbConnect);
     }
-    $userId = $dbConnect->query($sqlUsers)->fetch(PDO::FETCH_ASSOC);
-    $sqlInsertOrder = "INSERT INTO `orders_tbl` (`id_user`, `address`, `comment`, `pay`, `callback`) VALUES (?, ?, ?, ?, ?)";
-    $insertOrder = $dbConnect->prepare($sqlInsertOrder);
-    $insertOrder->bindParam(1, $userId, PDO::PARAM_INT);
-    $insertOrder->bindParam(2, $address);
-    $insertOrder->bindParam(3, $comment);
-    $insertOrder->bindParam(4, $pay);
-    $insertOrder->bindParam(5, $callback);
-//    $userId = ($userNew == "Y") ? $userNewId["id"] : $user["id"];
-    $address = $_POST["street"] . " / " . $_POST["home"] . " / " . $_POST["part"] . " / " . $_POST["appt"] . " / " . $_POST["floor"];
-    $comment = $_POST["comment"];
-    $pay = $_POST["pay"];
-    $callback = (isset($_POST["callback"])) ? "N" : "Y";
-    $resOrder = $insertOrder->execute();
+    $user = $dbConnect->query($sqlUsers)->fetch(PDO::FETCH_ASSOC);
+    insertOrder($_POST, $user["id"], $dbConnect);
     $dbConnect->commit();
 } catch (Exception $e) {
     $dbConnect->rollback();
     echo "Error: " . $e->getMessage();
 }
-$orderLastId = $dbConnect->query("SELECT `id` FROM orders_tbl WHERE `id_user` = '" . $userId . "' ORDER BY `id` DESC")->fetch(PDO::FETCH_ASSOC);
-$nameOrderFile = "orderid_" . $orderLastId["id"] . ".txt";
-$textOrder = "Ваш заказ будет доставлен по адресу: " . $address . "\n";
-$textOrder .= "DarkBeefBurger за 500 рублей, 1 шт. \n";
-$countOrders = $dbConnect->query("SELECT COUNT(*) as count FROM `orders_tbl` WHERE `id_user` = '" . $userId . "'")->fetch(PDO::FETCH_ASSOC);
-$textOrder .= "Спасибо - это ваш " . $countOrders["count"] . " заказ";
-file_put_contents("./orders/" . $nameOrderFile, $textOrder);
+$orderLastId = $dbConnect->query("SELECT `id` FROM orders_tbl WHERE `id_user` = '" . $user["id"] . "' ORDER BY `id` DESC")->fetch(PDO::FETCH_ASSOC);
+putFileOrder($orderLastId["id"], $user["id"], $dbConnect);
 echo "<h1>Your order is submited! Please wait!</h1>";
