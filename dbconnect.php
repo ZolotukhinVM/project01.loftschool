92ff28e95083902c4dc760cd354ecbab7b97e0cb
Viktor Zolotukhin <zolotukhinvm@ya.ru>
2019-03-03 00:07:44 +0300

    #13 add dbconnect

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+require "settings.php";
+try {
+    $dbConnect = new PDO(
+        sprintf("mysql:host=%s;dbname=%s;charset=%s", $settings["host"], $settings["name"], $settings["charset"]),
+        $settings["user"], $settings["password"], $options);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    die;
+}
